7ff04f5e25f651d2d18133bb5777b04a39997bb4
Eche200 <eecheconea@gmail.com>
2020-02-02 18:23:56 -0300

    agregado de css y modificacion de pagina principal

--- src/Components/PaginaPrincipal/PaginaPrincipal.js
+++ src/Components/PaginaPrincipal/PaginaPrincipal.js
@@ -1,58 +1,18 @@
 import React, {useState,Fragment} from 'react';
 import MenuPrincipal from '../MenuPrincipal/MenuPrincipal';
-import { Button ,Alert} from 'react-bootstrap';
+import { Button ,Navbar,Alert} from 'react-bootstrap';
+import './PaginaPrincipal.css';
 
 function PaginaPrincipal() {
-    const [count, setCount]  = useState(0);
-    const [menuAgregar, setMenuAgregar] = useState(false);
-    const [showAlert, setShowAlert] = useState(true);
-    const [colorAlert, setColorAlert]= useState("success");
-
-    function mostrar ()
-    {
-      /*
-      if(menuAgregar == false )
-      {
-        setMenuAgregar(true);
-      }
-      else
-      {
-        setMenuAgregar(false);
-      }*/
-      if(showAlert)
-      {
-        setShowAlert(false);
-      }
-      else
-      {
-        setShowAlert(true);
-      }
-    }
-    function cambiarColorAlert()
-    {
-      if(colorAlert === "success")
-      {
-        setColorAlert("danger");
-      }
-      else
-      {
-        setColorAlert("success");
-      }
-
-    }
-    
   return (
     <Fragment >
-      <Alert show = {showAlert} dismissible variant={colorAlert}>
-      <Alert.Heading>Esta todo bien!</Alert.Heading>
-      <p>
-        Toca el boton "Cambiar color" y veras lo que pasa.
-      </p>
-    </Alert>
-    <button onClick= {cambiarColorAlert}>cambiar color</button>
-
-      <button onClick= {mostrar}>ocultar alerta</button>
-        {menuAgregar && <MenuPrincipal></MenuPrincipal>}
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand className="nombredelaaplicacion" >Controlador de stock</Navbar.Brand>
+    </Navbar>
+    <Button variant ="outline-primary" className='botonesdemenu'> Agregar Producto</Button>
+    <Button variant ="outline-primary" className='botonesdemenu'> Quitar Producto</Button>
+    <Button variant ="outline-primary" className='botonesdemenu'> Listar Productos</Button>
+    <Button variant ="outline-primary" className='botonesdemenu'> Listar Proveedores</Button>
     </Fragment>
   );
 }
